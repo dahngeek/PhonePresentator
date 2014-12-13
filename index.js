ac0060f871e3189cc2ffcb186eb70b90beec5767
Dahngeek <me@dahngeek.com>
2014-12-13 14:41:05 -0600

    Ya funciona! :D node index para probarlo

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var keyboard = require('node_keyboard');
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  	socket.on('flecha', function (data){
+  		var comando = data.command;
+  		if (comando == 'prev') {
+  			console.log(comando);
+  			keyboard.press(keyboard.Key_Left);
+  			keyboard.release(keyboard.Key_Left);
+  		} else if (comando == 'next') {
+  			console.log(comando);
+  			keyboard.press(keyboard.Key_Up);
+  			keyboard.release(keyboard.Key_Up);
+  		};
+  	});
+   socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
